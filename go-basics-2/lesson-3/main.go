55068ac3490fad04324510f9808d3fb6d20f347a
Rishabh Verma <aryanverma2412@gmail.com>
2023-01-25 00:21:53 +0530

    restructured and added new lessons

---
+++ go-basics-2/lesson-3/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	// "%d":Integer
+	// "%f":Float
+	// "%t":Boolean
+	// "%s":String
+	// "%c":Character
+	// "%v":Value
+	// "%T":Type
+	// "%b":Binary
+	// "%x":Hexadecimal
+	// "%o":Octal
+	// "%U":Unicode
+	// "%e":Scientific Notation
+	// "%E":Scientific Notation
+	// "%p":Pointer
+	// "%q":Quoted String
+	// "%b":Base 2
+	// "\n"	:New Line
+	// "\t"	:Tab
+	// "\r"	:Carriage Return
+	
+}
