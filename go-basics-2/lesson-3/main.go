87f02501618bd647527019f1f9d6cf8f14d20acf
Rishabh Verma <aryanverma2412@gmail.com>
2023-01-26 12:16:47 +0530

    adding new lessons and concepts

--- go-basics-2/lesson-3/main.go
+++ go-basics-2/lesson-3/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// "%d":Integer
 	// "%f":Float
@@ -20,5 +22,12 @@ func main() {
 	// "\n"	:New Line
 	// "\t"	:Tab
 	// "\r"	:Carriage Return
-	
+
+	fmt.Printf("Hello, World!")
+	fmt.Printf("%9f\n", 3.14)
+	fmt.Printf("%.2f\n", 3.13333334)
+	fmt.Printf("%9.f\n", 3.1333333333333334)
+
+	sp1 := fmt.Sprintf("%9.f", 3.141333333333333333593)
+	fmt.Println(sp1)
 }
